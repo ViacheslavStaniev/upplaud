4af2b2b9697d6dbbe42459ec48b76f65a03584db
TIKAM CHAND <tikamchand06@gmail.com>
2023-03-08 19:10:10 +0530

    frontend build

---
+++ frontend/build/static/js/678.463603a9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpodasq=self.webpackChunkpodasq||[]).push([[678],{19678:function(s,a,e){e.r(a);var u=e(30413);a.default=u.g}}]);
+//# sourceMappingURL=678.463603a9.chunk.js.map
