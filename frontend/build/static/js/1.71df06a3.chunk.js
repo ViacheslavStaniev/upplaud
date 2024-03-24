2f6094361a0f2e895a75d37c8144eac8d7e8ff93
TIKAM CHAND <tikamchand06@gmail.com>
2024-03-24 13:57:36 +0530

    Frontend Build with Guest Pages Changes

---
+++ frontend/build/static/js/1.71df06a3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpdsq_frontend=self.webpackChunkpdsq_frontend||[]).push([[1],{29001:(e,n,s)=>{s.r(n),s.d(n,{default:()=>d});var r=s(80184);function d(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"User Automations"})})}}}]);
+//# sourceMappingURL=1.71df06a3.chunk.js.map
