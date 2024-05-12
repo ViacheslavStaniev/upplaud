72f3c1b69cee9a2788a1dfaf2e4ce66b16e122fd
TIKAM CHAND <tikamchand06@gmail.com>
2024-05-12 19:52:32 +0530

    frontend build

--- frontend/build/static/js/1.71df06a3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpdsq_frontend=self.webpackChunkpdsq_frontend||[]).push([[1],{29001:(e,n,s)=>{s.r(n),s.d(n,{default:()=>d});var r=s(80184);function d(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"User Automations"})})}}}]);
-//# sourceMappingURL=1.71df06a3.chunk.js.map
